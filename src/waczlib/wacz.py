dbe1e8bc732c51aef59da7a632c3f4f1002c22c6
Oskar Jauch <oskar.jauch@posteo.de>
2023-07-11 22:53:43 +0200

    Implement basic validation

--- src/waczlib/wacz.py
+++ src/waczlib/wacz.py
@@ -1,6 +1,11 @@
+import json
+import re
+
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Optional, Dict
+from typing import Optional, Dict, Generator
+from contextlib import contextmanager
+from zipfile import ZipFile
 
 
 class WaczArchive:
@@ -10,8 +15,43 @@ class WaczArchive:
     def validate(self) -> None:
         """
         Check if the given wacz archive is valid according to the specification
+
+        :raises InvalidWaczError: if the archive is invalid
         """
-        pass
+        with self._get_zip() as zip_file:
+            files = zip_file.namelist()
+
+            if 'datapackage.json' not in files:
+                raise InvalidWaczError("Does not contain datapackage.json")
+
+            with zip_file.open('datapackage.json') as datapackage_file:
+                try:
+                    datapackage = json.load(datapackage_file)
+                except json.JSONDecodeError:
+                    raise InvalidWaczError("datapackage.json is not a valid JSON file")
+
+            if 'profile' not in datapackage or datapackage['profile'] != 'data-package':
+                raise InvalidWaczError("profile must be set to 'data-package'")
+
+            if 'wacz_version' not in datapackage:
+                raise InvalidWaczError("wacz_version must be set")
+
+            if 'resources' not in datapackage or type(datapackage['resources']) != list \
+                    or len(datapackage['resources']) == 0:
+                raise InvalidWaczError("resources must be set to an non empty array")
+
+            archive_regex = re.compile(r'archive/.+\.warc(\.gz)?')
+            archive_files = [file for file in files if archive_regex.fullmatch(file)]
+            if len(archive_files) == 0:
+                raise InvalidWaczError("Wacz must contain at least one web archive")
+
+            index_regex = re.compile(r'indexes/.+\.cdx(\.gz)?')
+            index_files = [file for file in files if index_regex.fullmatch(file)]
+            if len(index_files) == 0:
+                raise InvalidWaczError("Wacz must contain at least one index file")
+
+            if 'pages/pages.jsonl' not in files:
+                raise InvalidWaczError('Does not contain pages/pages.jsonl')
 
     def verify_checksums(self) -> Dict[str, bool]:
         """
@@ -27,8 +67,16 @@ class WaczArchive:
         """
         pass
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"WaczArchive(path='{self._path}')"
+
+    @contextmanager
+    def _get_zip(self) -> Generator[ZipFile, None, None]:
+        try:
+            zip_file = ZipFile(self._path, 'r')
+            yield zip_file
+        finally:
+            zip_file.close()
 
 
 @dataclass
@@ -41,3 +89,8 @@ class WaczMetadata:
     software: Optional[str]
     mainPageUrl: Optional[str]
     mainPageDate: Optional[datetime]
+
+
+class InvalidWaczError(Exception):
+    def __init__(self, reason) -> None:
+        self.reason = reason
--- tests/test_wacz.py
+++ tests/test_wacz.py
@@ -1,6 +1,41 @@
-from waczlib.wacz import WaczArchive
+import pytest
+
+from waczlib.wacz import WaczArchive, InvalidWaczError
 
 
 def test_wacz_archive_repr():
     archive = WaczArchive("test.wacz")
     assert repr(archive) == "WaczArchive(path='test.wacz')"
+
+
+def test_validate_valid_wacz():
+    archive = WaczArchive("test_assets/valid-example.wacz")
+    archive.validate()
+
+
+def test_validate_invalid_datapackage():
+    archive = WaczArchive("test_assets/invalid-datapackage.wacz")
+    with pytest.raises(InvalidWaczError) as e_info:
+        archive.validate()
+    assert e_info.value.reason == "resources must be set to an non empty array"
+
+
+def test_validate_no_archives():
+    archive = WaczArchive("test_assets/no-archives.wacz")
+    with pytest.raises(InvalidWaczError) as e_info:
+        archive.validate()
+    assert e_info.value.reason == "Wacz must contain at least one web archive"
+
+
+def test_validate_no_datapackage():
+    archive = WaczArchive("test_assets/no-datapackage.wacz")
+    with pytest.raises(InvalidWaczError) as e_info:
+        archive.validate()
+    assert e_info.value.reason == "Does not contain datapackage.json"
+
+
+def test_validate_no_pages():
+    archive = WaczArchive("test_assets/no-pages.wacz")
+    with pytest.raises(InvalidWaczError) as e_info:
+        archive.validate()
+    assert e_info.value.reason == "Does not contain pages/pages.jsonl"
